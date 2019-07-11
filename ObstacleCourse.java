b9febc691703d6c45c208ea6255b589ac991765e
knightpowell <samuelbryant117@gmail.com>
2019-07-10 19:56:02 -0700

    final 2.0 submission

--- ObstacleCourse.java
+++ ObstacleCourse.java
@@ -8,118 +8,117 @@
 
 public class ObstacleCourse {
 
-  // Instance variables
-  protected char[][] course;
-  private int sRow;
-  private int sCol;
-  private int foundRow;
-  private int foundCol;
-  
-  // Constants (or you could use 'O' and '.' directly)
-  private static final char PART_OF_PATH = 'O';
-  private static final char TRIED = '.';
+	// Instance variables
+	protected char[][] course;
+	private int sRow;
+	private int sCol;
+	private int foundRow;
+	private int foundCol;
 
-  /**
-   * Initializes the 2d char array course.
-   */
-  public ObstacleCourse(int sRow, int sCol, char[][] course) {
-    this.sRow = sRow;
-    this.sCol = sCol;
-    this.course = course;
-    
-    // The default values in case there is no exit.
-    foundRow = -1;
-    foundCol = -1;
-  }
+	// Constants (or you could use 'O' and '.' directly)
+	private static final char PART_OF_PATH = 'O';
+	private static final char TRIED = '.';
 
-  // Returns the start column in the array
-  public int getStartColumn() {
-    return sCol;
-  }
+	/**
+	 * Initializes the 2d char array course.
+	 */
+	public ObstacleCourse(int sRow, int sCol, char[][] course) {
+		this.sRow = sRow;
+		this.sCol = sCol;
+		this.course = course;
 
-   // Returns the starting row in the array
-  public int getStartRow() {
-    return sRow;
-  }
+		// The default values in case there is no exit.
+		foundRow = -1;
+		foundCol = -1;
+	}
 
-  // Return the column of the solution
-  public int getExitColumn() {
-    return foundCol;
-  }
+	// Returns the start column in the array
+	public int getStartColumn() {
+		return sCol;
+	}
 
-  // Return the row of the solution
-  public int getExitRow() {
-    return foundRow;
-  }
+	// Returns the starting row in the array
+	public int getStartRow() {
+		return sRow;
+	}
 
-  // Returns a string representation of the array
-  public String toString() {
-    String result = "";
-    for (int i = 0; i < course.length; i++) {
-      for (int j = 0; j < course[0].length; j++) {
-        result += course[i][j];
-      }
-      result += "\n";
-    }
-    return result;
-  }
+	// Return the column of the solution
+	public int getExitColumn() {
+		return foundCol;
+	}
 
-  // This method is called by the user to begin the search for the one exit.
-  public void findTheExit() {
-    findExit(sRow, sCol);
-  }
+	// Return the row of the solution
+	public int getExitRow() {
+		return foundRow;
+	}
 
-  /**
-   * Finds the exit from the 2-D array. This method also must record the row and
-   * col where the exit was found
-   */
-  
-  
-  private boolean findExit(int row, int col) {
-    
-    // Do not forget to set the instance variable foundRow and 
-    // foundCol in this method when the exit is found.
-    
-	  boolean escaped = false;
-	  
-	  if(this.course[row][col] == ' ') {
-		  this.course[row][col] = TRIED;
-		  
-		  if(onBorder(row,col)) {
-			  this.foundRow = row;
-			  this.foundCol = col;
-			  escaped = true;
-		  }else {
-			  
-			  escaped = findExit(row+1,col);
-			  
-			  if(!escaped) {
-				  escaped = findExit(row,col+1);
-			  }
-			  
-			  if(!escaped) {
-				  escaped = findExit(row-1,col);
-			  }
-			  if(!escaped) {
-				  escaped = findExit(row,col-1);
-			  }
-			  
-		  }
-		  if(escaped == true) {
-			  this.course[row][col] = PART_OF_PATH;
-		  }
-	  }
-	  return escaped;
-  }
-  
-  private boolean onBorder(int row, int col) {
-	  if( row == 0 || row == this.course.length-1) {
-		  return true;
-	  }else if(col == 0 || col == this.course[0].length-1) {
-		  return true;
-	  }else {
-		  return false;
-	  }
-  }
+	// Returns a string representation of the array
+	public String toString() {
+		String result = "";
+		for (int i = 0; i < course.length; i++) {
+			for (int j = 0; j < course[0].length; j++) {
+				result += course[i][j];
+			}
+			result += "\n";
+		}
+		return result;
+	}
+
+	// This method is called by the user to begin the search for the one exit.
+	public void findTheExit() {
+		findExit(sRow, sCol);
+	}
+
+	/**
+	 * Finds the exit from the 2-D array. This method also must record the row and
+	 * col where the exit was found
+	 */
+
+	private boolean findExit(int row, int col) {
+
+		// Do not forget to set the instance variable foundRow and
+		// foundCol in this method when the exit is found.
+
+		boolean escaped = false;
+
+		if (this.course[row][col] == ' ') {
+			this.course[row][col] = TRIED;
+
+			if (onBorder(row, col)) {
+				this.foundRow = row;
+				this.foundCol = col;
+				escaped = true;
+			} else {
+
+				escaped = findExit(row + 1, col);
+
+				if (!escaped) {
+					escaped = findExit(row, col + 1);
+				}
+
+				if (!escaped) {
+					escaped = findExit(row - 1, col);
+				}
+				if (!escaped) {
+					escaped = findExit(row, col - 1);
+				}
+
+			}
+			if (escaped == true) {
+				this.course[row][col] = PART_OF_PATH;
+			}
+		}
+		return escaped;
+	}
+
+	private boolean onBorder(int row, int col) {
+		if (row == 0 || row == this.course.length - 1) {
+			return true;
+		} else if (col == 0 || col == this.course[0].length - 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
--- RecursionFun.java
+++ RecursionFun.java
@@ -1,15 +1,13 @@
 /**
- * Complete the seven methods methods in this file using recursion, no loops. 
- * Also complete these three methods inside LinkedList<E>:
- *   get(int)
- *   removeAll(E)
- *   duplicateAll(E)
- *   
- * Also complete one method in ObstacleCourse that uses recursive backtracking.
- *   findExit(int, int)
+ * Complete the seven methods methods in this file using recursion, no loops.
+ * Also complete these three methods inside LinkedList<E>: get(int) removeAll(E)
+ * duplicateAll(E)
  * 
- * Note: I believe the given unit test tests all methods. We will
- * be using this same for for correctness of code.
+ * Also complete one method in ObstacleCourse that uses recursive backtracking.
+ * findExit(int, int)
+ * 
+ * Note: I believe the given unit test tests all methods. We will be using this
+ * same for for correctness of code.
  * 
  * We will not be using code coverage for points.
  *
@@ -17,191 +15,184 @@
  */
 public class RecursionFun {
 
-  // Complete recursive method combinations that returns from n choose k.
-  // This method is described in 17_SimpleRecursion.pptx.
-  public int combinations(int n, int k) {
-	  if(k == 1) {
-		  return n;
-	  }
-	  if (n == k) {
-		  return 1;
-	  }
-	  return combinations(n-1,k-1) + combinations(n-1,k);
-  }
+	// Complete recursive method combinations that returns from n choose k.
+	// This method is described in 17_SimpleRecursion.pptx.
+	public int combinations(int n, int k) {
+		if (k == 1) {
+			return n;
+		}
+		if (n == k) {
+			return 1;
+		}
+		return combinations(n - 1, k - 1) + combinations(n - 1, k);
+	}
 
- 
-  // Complete recursive method intWithCommas that returns the argument as a String
-  // with commas in the correct places.
-  //
-  // intWithCommas(999) returns "999"
-  // intWithCommas(1234) returns "1,234"
-  // intWithCommas(1007) returns "1,007"
-  // intWithCommas(1023004567) returns "1,023,004,567"
-  //
-  // Precondition: n >= 0
-  public String intWithCommas(int n) {
-   
-	   String result = intWithCommasHelper(n,1,1000);
-	   
-	   return result;
-   }
-   
-   private String intWithCommasHelper(int n, double counter, double countCheck) {
-	   if(n == 0) {
-		   return "";
-	   }else {
-		   int quotient = n/10;
-		   int remainder = n % 10;
-		   String numberRemainder = "";
-		   if (remainder == 0)
-			   numberRemainder = "0";
-		   if (remainder == 1)
-			   numberRemainder = "1";
-		   if (remainder == 2)
-			   numberRemainder = "2";
-		   if (remainder == 3)
-			   numberRemainder = "3";
-		   if (remainder == 4)
-			   numberRemainder = "4";
-		   if (remainder == 5)
-			   numberRemainder = "5";
-		   if (remainder == 6)
-			   numberRemainder = "6";
-		   if (remainder == 7)
-			   numberRemainder = "7";
-		   if (remainder == 8)
-			   numberRemainder = "8";
-		   if (remainder == 9)
-			   numberRemainder = "9";
-		  
-		   
-		   if (counter == countCheck) {
-			   numberRemainder = numberRemainder + ",";
-		   		countCheck*=1000;
-		   }
-		   
-		   return intWithCommasHelper(quotient, counter*10, countCheck) + numberRemainder;
-	   
-   }
-   
-   
-  }
+	// Complete recursive method intWithCommas that returns the argument as a String
+	// with commas in the correct places.
+	//
+	// intWithCommas(999) returns "999"
+	// intWithCommas(1234) returns "1,234"
+	// intWithCommas(1007) returns "1,007"
+	// intWithCommas(1023004567) returns "1,023,004,567"
+	//
+	// Precondition: n >= 0
+	public String intWithCommas(int n) {
 
-  // Write recursive method reverseArray that reverses the array elements in a
-  // filled array of ints. Use recursion; do not use a loop. The following
-  // assertions must pass:
-  //
-  // int[] a = { 2, 4, 6 };
-  // rf.reverseArray(a);
-  // assertEquals(6, a[0]);
-  // assertEquals(4, a[1]);
-  // assertEquals(2, a[2]);
-  //
-  // Precondition: x.length > 0
-  public void reverseArray(int[] x) {
-    // You need a private helper that needs additional arguments.
-    // like x and x.length to keep track of array the indexes
-    // with no loop. Here is an example with the private helper
-    // immediately below.
-    reverseArray(x, 0, x.length);
-    
-    
-  }
+		String result = intWithCommasHelper(n, 1, 1000);
 
-  private void reverseArray(int[] x, int index, int len) {
-	int mid = len/2;
-    if(index == mid) {
-    	return;
-    }else {
-    	
-    		int temp = x[index];
-    		x[index] = x[len-index-1];
-    		x[len-index-1] = temp;
-    	
-    }
-    reverseArray(x, index+1, len);
-    
-  }
+		return result;
+	}
 
-  // Write recursive method arrayRange that returns the maximum
-  // integer minus the minimum integer in the filled array of 
-  // integers, Use recursion; do not use a loop. 
-  // Precondition: a.length > 0
-  public int arrayRange(int[] a) {
-    int min = a[0];
-    int max = a[0];
-    int tempResult = 0;
-    int result = arrayRangeHelper(min,max,tempResult,a,0,a.length);
-    return result;
-    
-  }
-  
-  private int arrayRangeHelper(int min,int max,int tempResult,int[] a,int index,int len) {
-	  
-	  
-	  if(index == len) {
-		  return tempResult;
-	  }else {
-		  if (a[index] < min) {
-			  min = a[index];
-		  }
-		  if (a[index] > max) {
-			  max = a[index];
-		  }
-	  }
-	  if((max-min)>tempResult) {
-		  tempResult = max-min;
-	  }
-	  return arrayRangeHelper(min,max,tempResult,a,index+1,len);
-  }
+	private String intWithCommasHelper(int n, double counter, double countCheck) {
+		if (n == 0) {
+			return "";
+		} else {
+			int quotient = n / 10;
+			int remainder = n % 10;
+			String numberRemainder = "";
+			if (remainder == 0)
+				numberRemainder = "0";
+			if (remainder == 1)
+				numberRemainder = "1";
+			if (remainder == 2)
+				numberRemainder = "2";
+			if (remainder == 3)
+				numberRemainder = "3";
+			if (remainder == 4)
+				numberRemainder = "4";
+			if (remainder == 5)
+				numberRemainder = "5";
+			if (remainder == 6)
+				numberRemainder = "6";
+			if (remainder == 7)
+				numberRemainder = "7";
+			if (remainder == 8)
+				numberRemainder = "8";
+			if (remainder == 9)
+				numberRemainder = "9";
 
-  // Return true if nums has all int elements in ascending order.
-  // If not isSorted, return false.
-  public boolean isSorted(int[] nums) {
-	  boolean sorted = isSortedHelper(nums, 0);
-	  return sorted;
-  }
-  
-  private boolean isSortedHelper(int[] nums, int index) {
-	 
-	  
-	  if (nums.length == 0 || nums.length == 1) {
-		  return true;
-	  }
-	  if (index != nums.length - 1) {
-		 
-		  if(nums[index] > nums[index+1]) {
-			  return false;
-		  }else {
-			  return isSortedHelper(nums, index+=1);
-		  }
-	  }else {
-		  return true;
-	  }
-  }
+			if (counter == countCheck) {
+				numberRemainder = numberRemainder + ",";
+				countCheck *= 1000;
+			}
 
-  // Complete method found to return true if search is found in strs.
-  // If not found, return false. Use equals, not ==.
-  public boolean found(String search, String[] strs) {
-	  boolean located = false;
-	  return foundHelper(search, strs, 0, located);
-  }
-  
-  private boolean foundHelper(String search, String[] strs, int index, boolean located) {
-	  if(strs.length == 0) {
-		  return false;
-	  }
-	  if(index == strs.length) {
-		  return located;
-	  }
-	  else {
-		  if(strs[index].equals(search)) {
-			 located = true;
-			 return located;
-		  }else {
-			  located = false;
-		  }
-		  return foundHelper(search, strs, index+=1,located);
-	  }
-  }
+			return intWithCommasHelper(quotient, counter * 10, countCheck) + numberRemainder;
+
+		}
+
+	}
+
+	// Write recursive method reverseArray that reverses the array elements in a
+	// filled array of ints. Use recursion; do not use a loop. The following
+	// assertions must pass:
+	//
+	// int[] a = { 2, 4, 6 };
+	// rf.reverseArray(a);
+	// assertEquals(6, a[0]);
+	// assertEquals(4, a[1]);
+	// assertEquals(2, a[2]);
+	//
+	// Precondition: x.length > 0
+	public void reverseArray(int[] x) {
+		// You need a private helper that needs additional arguments.
+		// like x and x.length to keep track of array the indexes
+		// with no loop. Here is an example with the private helper
+		// immediately below.
+		reverseArray(x, 0, x.length);
+
+	}
+
+	private void reverseArray(int[] x, int index, int len) {
+		int mid = len / 2;
+		if (index == mid) {
+			return;
+		} else {
+
+			int temp = x[index];
+			x[index] = x[len - index - 1];
+			x[len - index - 1] = temp;
+
+		}
+		reverseArray(x, index + 1, len);
+
+	}
+
+	// Write recursive method arrayRange that returns the maximum
+	// integer minus the minimum integer in the filled array of
+	// integers, Use recursion; do not use a loop.
+	// Precondition: a.length > 0
+	public int arrayRange(int[] a) {
+		int min = a[0];
+		int max = a[0];
+		int tempResult = 0;
+		int result = arrayRangeHelper(min, max, tempResult, a, 0, a.length);
+		return result;
+
+	}
+
+	private int arrayRangeHelper(int min, int max, int tempResult, int[] a, int index, int len) {
+
+		if (index == len) {
+			return tempResult;
+		} else {
+			if (a[index] < min) {
+				min = a[index];
+			}
+			if (a[index] > max) {
+				max = a[index];
+			}
+		}
+		if ((max - min) > tempResult) {
+			tempResult = max - min;
+		}
+		return arrayRangeHelper(min, max, tempResult, a, index + 1, len);
+	}
+
+	// Return true if nums has all int elements in ascending order.
+	// If not isSorted, return false.
+	public boolean isSorted(int[] nums) {
+		boolean sorted = isSortedHelper(nums, 0);
+		return sorted;
+	}
+
+	private boolean isSortedHelper(int[] nums, int index) {
+
+		if (nums.length == 0 || nums.length == 1) {
+			return true;
+		}
+		if (index != nums.length - 1) {
+
+			if (nums[index] > nums[index + 1]) {
+				return false;
+			} else {
+				return isSortedHelper(nums, index += 1);
+			}
+		} else {
+			return true;
+		}
+	}
+
+	// Complete method found to return true if search is found in strs.
+	// If not found, return false. Use equals, not ==.
+	public boolean found(String search, String[] strs) {
+		boolean located = false;
+		return foundHelper(search, strs, 0, located);
+	}
+
+	private boolean foundHelper(String search, String[] strs, int index, boolean located) {
+		if (strs.length == 0) {
+			return false;
+		}
+		if (index == strs.length) {
+			return located;
+		} else {
+			if (strs[index].equals(search)) {
+				located = true;
+				return located;
+			} else {
+				located = false;
+			}
+			return foundHelper(search, strs, index += 1, located);
+		}
+	}
 }
